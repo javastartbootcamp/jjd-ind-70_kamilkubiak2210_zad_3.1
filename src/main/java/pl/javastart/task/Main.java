a6cbd5393057af8f569c4debf2595346fc89d0d6
kamilkubiak2210 <kamil.kubiak4@outlook.com>
2023-03-29 17:37:15 +0200

    Drugi commit

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,16 +3,8 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-        Monitor monitor1 = new Monitor();
-        monitor1.producer = "Asus";
-        monitor1.model = "VG248QG";
-        monitor1.screenDiagonal = 24;
-        monitor1.refreshRate = 165;
-        Monitor monitor2 = new Monitor();
-        monitor2.producer = "Acer";
-        monitor2.model = "V206HQL";
-        monitor2.screenDiagonal = 19.5;
-        monitor2.refreshRate = 60;
+        Monitor monitor1 = new Monitor("Asus", "VG248QG", 24, 165);
+        Monitor monitor2 = new Monitor("Acer", "V206HQL", 19.5, 60);
         System.out.println("Monitor: " + monitor1.producer + " " + monitor1.model + ", przekątna ekranu: " + monitor1.screenDiagonal
                 + "\", częstotliwość odświeżania ekranu: " + monitor1.refreshRate + "Hz");
         System.out.println("Monitor: " + monitor2.producer + " " + monitor2.model + ", przekątna ekranu: " + monitor2.screenDiagonal
